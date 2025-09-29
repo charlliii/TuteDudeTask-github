efe077235d3c36b92fea11eae5780c55b37f5c67
charlliii <charlliii8003@gmail.com>
2025-09-29 13:24:28 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+#Perform Basic Mathematical Operations
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+print(f"Addition: {a+b}")
+print(f"Subtraction: {a-b}")
+print(f",Multiplication: {a*b}")
+print(f"Division: {a/b}")
