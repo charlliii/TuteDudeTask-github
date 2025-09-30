c13b353645da966909d67e90e094d810b3d222d5
charlliii <charlliii8003@gmail.com>
2025-09-30 21:50:24 +0530

    Delete task1.py

--- task1.py
+++
@@ -1,7 +0,0 @@
-#Perform Basic Mathematical Operations
-a=int(input("Enter the first number: "))
-b=int(input("Enter the second number: "))
-print(f"Addition: {a+b}")
-print(f"Subtraction: {a-b}")
-print(f",Multiplication: {a*b}")
-print(f"Division: {a/b}")
