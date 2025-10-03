a4af007f3020f0acdaac75a7761c7006065dc09c
charlliii <charlliii8003@gmail.com>
2025-10-03 13:43:51 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,5 +1,5 @@
 from math import *
 num=int(input("Enter a number: "))
-print(sqrt(num))
-print(log(num))
-print(sin(num))
+print(sqrt(num)) #Square root of the number
+print(log(num)) #Natural logarithm (log base e) of the number
+print(sin(num)) #Sine of the number (in radians)
